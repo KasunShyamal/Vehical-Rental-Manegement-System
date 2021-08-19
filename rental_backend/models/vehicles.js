fb5e436828dcfa11b6972b596c5447c7dd36731e
HP <it20125066@my.sliit.lk>
2021-08-19 20:24:04 +0530

    add vehicle model

---
+++ rental_backend/models/vehicles.js
@@ -0,0 +1,47 @@
+const mongoose=require('mongoose');
+///const Schema=mongoose.Schema;
+const vehicleSchema = new mongoose.Schema({
+    vehicle_id:{
+        type: String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    vehicle_name:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    vehicle_model:{
+        type: String,
+        trim: true,
+        required: true
+    },
+    sheet_count:{
+        type: Number,
+        required: true
+    },
+    amenties:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    images:{
+        type: Object,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    checked:{
+        type: Boolean,
+        default: false
+    },
+      postDate:{type:Date,default:Date.now}
+    })
+const Vehicle=mongoose.model("vehicles",vehicleSchema);
+module.exports=Vehicle;
