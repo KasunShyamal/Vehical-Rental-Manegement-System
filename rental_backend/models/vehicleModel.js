97854ddc332966e9e9993b273101a7413e1f4b40
HP <it20125066@my.sliit.lk>
2021-08-22 17:01:29 +0530

    add

--- rental_backend/models/vehicleModel.js
+++ rental_backend/models/vehicleModel.js
@@ -1,12 +1,7 @@
 const mongoose=require('mongoose');
 ///const Schema=mongoose.Schema;
 const vehicleSchema = new mongoose.Schema({
-    vehicle_id:{
-        type: String,
-        unique: true,
-        trim: true,
-        required: true
-    },
+   
     vehicle_name:{
         type: String,
         trim: true,
@@ -29,18 +24,12 @@ const vehicleSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    images:{
-        type: Object,
-        required: true
-    },
+    
     category:{
         type: String,
         required: true
     },
-    checked:{
-        type: Boolean,
-        default: false
-    },
+  
       postDate:{type:Date,default:Date.now}
     })
 const Vehicle=mongoose.model("Vehicles",vehicleSchema);
