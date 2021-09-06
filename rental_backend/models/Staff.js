8469e91a5385eeba508b2c753dd0d5ce189fc350
Isuru362 <isuruprabhath6@gmail.com>
2021-09-06 21:48:53 +0530

    staff module

--- rental_backend/models/Staff.js
+++ rental_backend/models/Staff.js
@@ -6,17 +6,60 @@ const staffSchema = new Schema({
         type : String,
         required: true
     },
-    age:{
-        type : Number,
+    DoB : {
+        type : date,
         required: true
     },
-    gender:{
+    NIC : {
+        type : String,
+        required : true,
+    },
+    Address : {
+        type : String,
+        required : true,
+    },
+    Email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    Phone : {
+        type : Number,
+        required : true,
+        unique : true,
+    },
+    gender : {
         type : String,
         required: true
+    },
+    ID : {
+        type : String,
+        required : true,
+    },
+    JobTitle : {
+        type : String,
+        required : true,
+    },
+    Experience : {
+        type : Number,
+        required : true,
+    },
+    HireDate : {
+        type : Date,
+        required : true,
+    },
+    UserName : {
+        type : String,
+        required :true,
+    },
+    Password : {
+        type : String,
+        required :true,
     }
+
 
 })
 
-const Student = mongoose.model("Student",staffSchema);
+const Student = mongoose.model("Staff",staffSchema );
 
-module.exports = Student;
+module.exports = Staff;
