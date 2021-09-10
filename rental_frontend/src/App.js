04a28b7380d6bfac6cfcc093d53df4e7808464f4
KasunShyamal <kshyamal48@gmail.com>
2021-09-10 13:11:54 +0530

    interface desinged and some packaged added

--- rental_frontend/src/App.js
+++ rental_frontend/src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Landingpage from './Screens/LandingPage/Landingpage';
-
+import { BrowserRouter, Route } from "react-router-dom"
+import Registerpage from './Screens/Registerpage/Registerpage';
+import Loginpage from './Screens/LoginPage/Loginpage';
 
 
 
 
 const App = () =>(
-  <>
+  document.title = "Route Master",
+  <BrowserRouter>
       <Header />
       <main>
-        <Landingpage />
+        <Route path='/' component ={Landingpage} exact/>
+        <Route path='/login' component ={Loginpage} exact/>
+        <Route path='/register' component ={Registerpage} exact/>
       </main>
       <Footer />
 
-  </>
+  </BrowserRouter>
 )
   
 
