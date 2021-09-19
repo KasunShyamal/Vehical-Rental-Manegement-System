f78f709a6fe97580021959308a5f862fa2e40d49
Kaveesha <kaveesha856@gmail.com>
2021-09-19 12:24:21 +0530

    backend done

--- rental_backend/service.js
+++ rental_backend/service.js
@@ -6,12 +6,13 @@ const bodyParser = require("body-parser");
 const nodemon = require("nodemon");
 const app = express();
 require("dotenv").config();
-
+const orderRoutes = require('./routes/orderRoutes');
 
 const PORT = process.env.PORT || 8092;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 
 const URL = process.env.MONGODB_URL ;
 
@@ -26,6 +27,10 @@ const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("MongoDb connection success!");
 })
+
+app.use('/api/orders', orderRoutes);
+
+
 
 app.listen(PORT, () =>{
     console.log(`Server is up and running in port no : ${PORT}`);
