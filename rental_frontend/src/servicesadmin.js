01ee51619323f906cfe2b45cda660fe820421026
imhyva <it20128890@my.sliit.lk>
2021-09-20 15:23:47 +0530

    add

---
+++ rental_frontend/src/servicesadmin.js
@@ -0,0 +1,32 @@
+import React,{usestatus} from"react"
+
+export default function Addservicedetails()
+
+return(
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Dos</th>
+      <th scope="col">Mileageatservice</th>
+      <th scope="col">PerformedBy</th>
+      <th scope="col">Note</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+     
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2"></td>
+     
+    </tr>
+  </tbody>
+</table>
+)
